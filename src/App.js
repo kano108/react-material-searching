7ddaaaf97b32dc20e193def2949c11fda4581fb4
akshit <akshit.kanani.contractor@eqonex.com>
2022-07-06 16:05:07 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import SearchBar from "material-ui-search-bar";
+
+const getdata = () => {
+  const list = localStorage.getItem("kishan");
+  if (list) {
+    return JSON.parse(localStorage.getItem("kishan"));
+  } else {
+    return [];
+  }
+};
 
 function App() {
+  const [input, setInput] = useState("");
+  const [item, setItems] = useState(getdata());
+  const [searchInput ,setSearchInput] = useState("");
+  const submit = (e) => {
+    e.preventDefault();
+    const allData = {
+      Id: new Date().getTime().toString(),
+      Name: input,
+    };
+    setItems([...item, allData]);
+    localStorage.setItem("kishan", JSON.stringify([...item, allData]));
+    setInput("");
+  };
+
+  // const requestsearch = (searchedVal = String) => {
+
+  //   const filter_value = item.filter((elem) => {
+  //     return elem.Name.toLowerCase().includes(searchedVal.toLowerCase());
+  //   });
+  //   setItems(filter_value);
+  // };
+
+  const requestsearch = (searchedVal = String) => {
+    const filter_value = item.filter((elem) => {
+      return elem.Name.toLowerCase().includes(searchedVal.toLowerCase());
+    });
+    setItems(filter_value);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Material-Ui Searching</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <br />
+      <br />
+      <button type="submit" onClick={(e) => submit(e)}>
+        Submit
+      </button>
+      <br />
+      <br />
+      <SearchBar value={searchInput} onChange={(searchVal) => requestsearch(searchVal)} />
+      {
+       item.map((elem) => {
+          return <h1>{elem.Name}</h1>;
+        })}
     </div>
   );
 }
